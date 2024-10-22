710829acb92d1f06fb66479cccfe0976ab0e3142
amandalocher <akent22000@gmail.com>
2024-10-22 12:58:23 -0400

    initial commit

---
+++ src/components/CharacterForm.js
@@ -0,0 +1,10 @@
+
+function CharacterForm() {
+  return (
+    <div className="App">
+      <h1>Form</h1>
+    </div>
+  );
+}
+
+export default CharacterForm;
--- src/index.js
+++ src/index.js
@@ -1,15 +1,32 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
 import './index.css';
-import App from './App';
+import Home from "./components/Home"
+import CharacterForm from "./components/CharacterForm"
+import Profile from "./components/Profile"
+
 import reportWebVitals from './reportWebVitals';
 
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home />
+  },
+  {
+    path: "/form",
+    element: <CharacterForm />
+  },
+  {
+    path: "/profile",
+    element: <Profile />
+  }
+]);
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+root.render(<RouterProvider router={router} />);
+
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
